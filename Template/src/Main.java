39613c44fc56c1098ef924d1d526f71e97827536
mkujun <kujundzic.marko@hotmail.com>
2024-12-01 08:30:13 +0100

    template

--- Template/src/Main.java
+++ Template/src/Main.java
@@ -1,5 +1,10 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Path filePath = Paths.get("Template/src/example.txt");
+        List<String> lines = Utils.readFile(filePath);
     }
 }
